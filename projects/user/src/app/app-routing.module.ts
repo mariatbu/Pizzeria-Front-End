a0358979f17003da5824ade224602f6e70209397
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-20 11:48:07 +0200

    login route added

--- projects/user/src/app/app-routing.module.ts
+++ projects/user/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
   },
 ];
 
--- projects/user/src/app/user/user-routing.module.ts
+++ projects/user/src/app/user/user-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
 import { UserComponent } from './user.component';
 
 const routes: Routes = [
   {
     path:'', component: UserComponent
+  },
+  {
+    path:'login', component: LoginComponent
   }
 ];
 
