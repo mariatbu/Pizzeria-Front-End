b4a14a8094808fa89bee1a4b98284bc543ec975e
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-24 08:43:32 +0200

    authorize done

--- projects/ingredient/src/app/service/ingredient.service.ts
+++ projects/ingredient/src/app/service/ingredient.service.ts
@@ -3,6 +3,7 @@ import { HttpClient} from '@angular/common/http'
 import { Ingredient } from '../ingredient/ingredient';
 import { environment } from '../../../../../src/environments/environment';
 import { Observable } from 'rxjs';
+import { Authorize } from 'projects/core/src/public-api';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,7 @@ export class IngredientService {
      
    }
 
+   @Authorize()
    getAll(): Observable<Ingredient []>{
       return <Observable<Ingredient[]>> this.http.get(`${this.url}/ingredients`);
    }
