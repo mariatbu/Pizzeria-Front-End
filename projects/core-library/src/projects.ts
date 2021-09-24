14fd3c6f57bc02aa7fc85b3eb9bf5bf47ad8c377
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-24 08:41:38 +0200

    deleted core-library

--- projects/core-library/src/projects.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of core-library
- */
-
-export * from './lib/core-library.service';
-export * from './lib/core-library.component';
-export * from './lib/core-library.module';
