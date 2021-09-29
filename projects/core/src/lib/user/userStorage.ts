dd222d706bfe29ce70217e2aa963116a2388fa54
JaviQuinho <javier.qg@hotmail.com>
2021-09-29 09:08:40 +0200

    Create menu

---
+++ projects/core/src/lib/user/userStorage.ts
@@ -0,0 +1,20 @@
+import { UserDto } from "./userDto";
+const key = "user";
+
+class UserStorage{
+    private userDTO: UserDto|any ={};
+    addUser(userDTO: UserDto){
+        this.userDTO = userDTO;
+        return new Promise((resolve, reject)=>{
+            resolve('');
+        })
+    }
+    //TODO generic TypeScript
+    getUser():Promise<UserDto | any>{
+        return new Promise((resolve, reject)=>{
+            //TODO meter token
+            resolve(this.userDTO);
+        })
+    }
+}
+export default new UserStorage();
