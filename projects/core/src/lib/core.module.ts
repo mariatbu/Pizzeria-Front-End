d67afc39e8c64f382362d6f9df4ae7dce5c1b4b1
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-29 14:00:50 +0200

    Register works

--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 
@@ -8,6 +9,7 @@ import { InputComponent } from './components/input/input.component';
     InputComponent
   ],
   imports: [
+    ReactiveFormsModule,
   ],
   exports: [
     ButtonComponent,
