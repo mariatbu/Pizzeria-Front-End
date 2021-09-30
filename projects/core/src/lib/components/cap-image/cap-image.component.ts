afcc03c885f63e0d6172d4fe96e0d57b1e927eaa
mariatbu <maria.turpin.buendia98@gmail.com>
2021-09-30 12:31:16 +0200

    Register working with css but not the image

---
+++ projects/core/src/lib/components/cap-image/cap-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-cap-image',
+  templateUrl: './cap-image.component.html',
+  styleUrls: ['./cap-image.component.css']
+})
+export class CapImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
