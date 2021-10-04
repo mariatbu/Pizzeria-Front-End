3cc82e59f4f738955f951e1ec653abc407963fc6
JaviQuinho <javier.qg@hotmail.com>
2021-10-04 12:18:47 +0200

    Add socialUser Google and Facebook

--- projects/core/src/lib/components/menu/menu.component.ts
+++ projects/core/src/lib/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'lib-menu',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  userLogged: SocialUser = new SocialUser;
+  isLogged: boolean = false;
+
+  constructor(private authService: SocialAuthService, private router: Router) { 
+
+  }
 
   ngOnInit(): void {
+    this.authService.authState.subscribe(
+      data => {
+        this.userLogged = data;
+        this.isLogged = (this.userLogged != null)
+      }
+    );
+  }
+
+  logout(): void {
+    this.authService.signOut();
+    this.router.navigate(['/login']);
   }
 
 }
--- projects/user/src/app/app.module.ts
+++ projects/user/src/app/app.module.ts
@@ -9,11 +9,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+
+//Social login
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,9 +27,29 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    CoreModule
+    CoreModule,
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '157779970867-kr2r2tub55sj91ch782j8kd66iupe22b.apps.googleusercontent.com'
+          )
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider(
+            '486703449812965'
+          )
+        }
+      ]
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
